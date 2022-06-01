bbf5c4bad3e52d11748255d989bf4f92ff06ecf6
lgh1024 <lj910039885@gmail.com>
2022-06-01 11:09:03 +0800

    vue

--- src/main.js
+++ src/main.js
@@ -4,11 +4,12 @@ import router from './router'
 import './plugins/element.js'
 // 导入全局样式表
 import './assets/css/global.css'
+import TreeTable from 'vue-table-with-tree-grid'
+
 // 导入axios
 import axios from 'axios'
 // 配置请求根路径
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/' // (本地)
-// axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1/' // (线上)
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/' 
 // 设置请求拦截器
 axios.interceptors.request.use(config => {
   // console.log(config)
@@ -20,6 +21,8 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
 
 new Vue({
   router,
